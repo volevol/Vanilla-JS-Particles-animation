4f73b8f33f39383faa2cd586d12de37febbb66a1
Ivan Fedorusov <ifedorusov@gmail.com>
2020-03-25 10:18:47 +0200

    Add more particles with different sizes

--- script.js
+++ script.js
@@ -1,63 +1,69 @@
-(() => {
-    const cnv = document.querySelector(`canvas`);
-    const ctx = cnv.getContext(`2d`);
+const cnv = document.querySelector(`canvas`);
+const ctx = cnv.getContext(`2d`);
 
-    const cfg = {
-        orbsCount: 40,
-        minVelocity: 0.2
-    };
+const cfg = {
+    orbsCount: 400,
+    minVelocity: 0.2,
+    ringsCount: 10
+};
 
-    let cw, ch, cx, cy, ph;
-    function resize() {
-        cw = cnv.width = innerWidth;
-        ch = cnv.height = innerHeight;
-        cx = cw * 0.5;
-        cy = ch * 0.5;
-        ph = cy * 0.4;
-    }
-    resize();
-    window.addEventListener(`resize`, resize);
+let cw, ch, cx, cy, ph;
+function resize() {
+    cw = cnv.width = innerWidth;
+    ch = cnv.height = innerHeight;
+    cx = cw * 0.5;
+    cy = ch * 0.5;
+    ph = cy * 0.4;
+}
+resize();
+window.addEventListener(`resize`, resize);
 
-    class Orb {
-        constructor() {
-            this.size = 5;
-            this.angle = Math.random() * 360;
-            this.velocity = cfg.minVelocity + Math.random() * cfg.minVelocity;
-        }
-
-        refresh() {
-            let radian = (this.angle * Math.PI) / 180;
-
-            let cos = Math.cos(radian);
-            let sin = Math.sin(radian);
-
-            let x = cx + cos * ph;
-            let y = cy + sin * ph;
-
-            ctx.fillStyle = `red`;
-            ctx.beginPath();
-            ctx.arc(x, y, this.size, 0, 2 * Math.PI);
-            ctx.fill();
-
-            this.angle = (this.angle + this.velocity) % 360;
-        }
+class Orb {
+    constructor() {
+        this.size = Math.random() * 5 + 2;
+        this.angle = Math.random() * 360;
+        this.radius =
+            (((Math.random() * cfg.ringsCount) | 0) * ph) / cfg.ringsCount;
+        this.impact = this.radius / ph;
+        this.velocity = cfg.minVelocity + Math.random() * cfg.minVelocity;
     }
 
-    let orbsList = [];
-    function createStardust() {
-        for (let i = 0; i < cfg.orbsCount; i++) {
-            orbsList.push(new Orb());
-        }
+    refresh() {
+        let radian = (this.angle * Math.PI) / 180;
+
+        let cos = Math.cos(radian);
+        let sin = Math.sin(radian);
+
+        let x = cx + cos * (ph + this.radius);
+        let y = cy + sin * (ph + this.radius);
+
+        let optic = sin * this.size * this.impact * 0.7;
+
+        let size = this.size + optic;
+
+        ctx.fillStyle = `red`;
+        ctx.beginPath();
+        ctx.arc(x, y, size, 0, 2 * Math.PI);
+        ctx.fill();
+
+        this.angle = (this.angle + this.velocity) % 360;
     }
-    createStardust();
+}
 
-    function loop() {
-        requestAnimationFrame(loop);
-
-        ctx.fillStyle = `rgb(22,22,22)`; // Очищает холст
-        ctx.fillRect(0, 0, cw, ch);
-
-        orbsList.map(e => e.refresh()); // Заново заполняет
+let orbsList = [];
+function createStardust() {
+    for (let i = 0; i < cfg.orbsCount; i++) {
+        orbsList.push(new Orb());
     }
-    loop();
-})();
+}
+createStardust();
+
+function loop() {
+    requestAnimationFrame(loop);
+
+    ctx.fillStyle = `rgb(22,22,22)`; // Очищает холст
+    ctx.fillRect(0, 0, cw, ch);
+
+    orbsList.map(e => e.refresh()); // Заново заполняет
+}
+loop();
